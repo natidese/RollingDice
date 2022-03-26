d590fdfba09e3119b2644e52a0acc35eae687dd5
natidese <natidese0@gmail.com>
2022-03-26 23:37:33 +0300

    new game btn

--- script.js
+++ script.js
@@ -10,62 +10,84 @@ let current1El = document.getElementById("current--0");
 let current2El = document.getElementById("current--1");
 let btnNewGame = document.querySelector(".btn--new");
 
+let scores;
+let currentScore;
+let activePlayer;
+let playing;
 
-score0El.textContent = 0;
-score1El.textContent = 0;
-let playing = true;
-diceEl.classList.add("hidden");
+const init = function () {
+   scores = [0, 0];
+   currentScore = 0;
+   activePlayer = 0;
+   playing = true;
 
-let scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current1El = 0;
+  current2El = 0;
+  currentScore = 0;
+  activePlayer = 0;
 
-let switchPlayer = function(){
+  diceEl.classList.add("hidden");
+  player1.classList.remove("player--winner");
+  player2.classList.remove("player--winner"); 
+  player1.classList.add("player--active");
+  player2.classList.remove("player--active");
+};
+
+init();
+
+let switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   currentScore = 0;
   activePlayer = activePlayer === 0 ? 1 : 0;
-    player1.classList.toggle("player--active");
-    player2.classList.toggle("player--active");
-}
+  player1.classList.toggle("player--active");
+  player2.classList.toggle("player--active");
+};
 
 btnRoll.addEventListener("click", () => {
-  if (playing){
-  let rollDice = Math.trunc(Math.random() * 6) + 1;
-  console.log(rollDice);
-  diceEl.classList.remove("hidden");
-  diceEl.src = `dice-${rollDice}.png`;
+  if (playing) {
+    let rollDice = Math.trunc(Math.random() * 6) + 1;
+    console.log(rollDice);
+    diceEl.classList.remove("hidden");
+    diceEl.src = `dice-${rollDice}.png`;
 
-  if (rollDice !== 1) {
-    currentScore += rollDice;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    switchPlayer()
-  }}
-
+    if (rollDice !== 1) {
+      currentScore += rollDice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
+  }
 });
 
-btnHold.addEventListener("click", function(){
-if (playing){
-scores[activePlayer] += currentScore;
-document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
-if(scores[activePlayer]>= 20){
-  playing = false;
-  diceEl.classList.add("hidden");
-  document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
-  document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
-  if (activePlayer === 0 ){
-  alert(`game over player 1 won`)
-}
-else{
-  alert(`game over player 2 won`)
-}
-}
-switchPlayer()
-}
-})
+btnHold.addEventListener("click", function () {
+  if (playing) {
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    if (scores[activePlayer] >= 100) {
+      playing = false;
+      diceEl.classList.add("hidden");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add("player--winner");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove("player--active");
+      if (activePlayer === 0) {
+        alert(`game over player 1 won`);
+      } else {
+        alert(`game over player 2 won`);
+      }
+    }
+    switchPlayer();
+  }
+});
 
+btnNewGame.addEventListener("click", init);
 
-btnNewGame.addEventListener( 'click', function(){
-  location.reload();
-})
+// btnNewGame.addEventListener( 'click', function(){
+//   location.reload();
+// })
